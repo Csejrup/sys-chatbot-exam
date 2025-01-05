a5050d65711eccbe52b4ed388feabe00f6266afb
Patrick Jakobsen Jahn <55191533+PatrickJahn@users.noreply.github.com>
2025-01-05 18:34:24 +0100

    Added tests for chat servcie

--- SysChatBot/ChatService/Services/ai/AiService.cs
+++ SysChatBot/ChatService/Services/ai/AiService.cs
@@ -1,6 +1,8 @@
 using Chatbot;
 using ChatService.Models;
+using ChatService.Utils;
 using Grpc.Net.Client;
+using Polly.Retry;
 using SysChatBot.Shared.Models;
 
 namespace ChatService.Services.ai;
@@ -9,18 +11,26 @@ public class AiService : IAiService
 {
     
     private readonly ChatbotService.ChatbotServiceClient _client;
+    private readonly AsyncRetryPolicy _retryPolicy;
 
-    public AiService(string grpcServerUrl)
+    public AiService(string grpcServerUrl, ChatbotService.ChatbotServiceClient? client = null )
     {
         var channel = GrpcChannel.ForAddress(grpcServerUrl);
-        _client = new ChatbotService.ChatbotServiceClient(channel);
+        _client = client ?? new ChatbotService.ChatbotServiceClient(channel);
+        _retryPolicy = PollyRetryPolicy.CreateRetryPolicy();
+
     }
     
     public async Task<string> GetAIResponseAsync(string userMessage, List<ChatMessage> conversationHistory)
     {
         // Create a request and call the gRPC server
         var request = new ChatbotRequest { Message = userMessage };
-        var response = await _client.GetResponseAsync(request);
-        return response.Message;
+        return await _retryPolicy.ExecuteAsync(async () =>
+        {
+            var response = await _client.GetResponseAsync(request);
+            return response.Message;
+        });
     }
 }
+
+
